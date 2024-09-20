e26676f73ead1e30e813e1f2b288e9a66ba4e0e7
Alvaro Lopez de Ocariz <a.lopezdeocarizde@opendeusto.es>
2024-09-20 08:37:37 +0200

    gobierno

--- Prueba-git/src/HolaMundo.java
+++ Prueba-git/src/HolaMundo.java
@@ -2,6 +2,6 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hasta Lugo!!!");
-		System.out.println("Esto es otro mensaje");
+		System.out.println("fuego al gobierno");
 	}
 }
